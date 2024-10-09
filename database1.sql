8792bd5c9c95a4aa0baa196dc5a413d649a19dec
Kanishka Sharma <sharmakanishkasharma1966deeghi@gmail.com>
2024-10-09 11:36:51 +0530

    Add files via upload

---
+++ database1.sql
@@ -0,0 +1,6 @@
+/*
+-- Query: select*from empdata
+LIMIT 0, 5000
+-- Date: 2024-10-01 12:20
+*/
+INSERT INTO `` (`Id`,`Name`,`Email_id`,`Phoneno`,`Address`,`Post`,`Salary`) VALUES (1,'Kanishka Sharma','sharmakanishkasharma1966deeghi@gmail.com',9711841013,'Noida','Data Analyst',70000);
